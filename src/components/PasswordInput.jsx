e28903fc6e2765d429d5ea34b8cf0400048a2947
Dmitry Pavlov <dimapav1985@mail.ru>
2022-09-28 22:41:57 +0700

    fix: improved PasswordInput comonent

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,31 +1,25 @@
 import { useState } from "react";
 
-const PasswordInput = ({ isValue, onChange }) => {
-  const [typeInput, setTypeInput] = useState("password");
-  const [passIcon, setPassIcon] = useState("pass-icon_type_unvisible");
+const PasswordInput = ({ value, onChange }) => {
+  const [isHiden, setIsHiden] = useState(true);
 
-  const toggleViewPassword = () => {
-    if (typeInput === "password") {
-      setTypeInput("text");
-      setPassIcon("pass-icon_type_visible");
-    } else {
-      setTypeInput("password");
-      setPassIcon("pass-icon_type_unvisible");
-    }
-  };
+  const toggleViewPassword = () => setIsHiden(state => !state);
+
+  const type = isHiden ? "password" : "text";
+  const iconClass = isHiden ? "pass-icon_type_unvisible" : "pass-icon_type_visible";
 
   return (
     <>
       <input
-        type={typeInput}
-        value={isValue}
+        type={type}
+        value={value}
         name="password"
         placeholder="Пароль"
         required
         onChange={onChange}
         className="pass-input"
       />
-      <span className={`pass-icon ${passIcon}`} onClick={toggleViewPassword} />
+      <span className={`pass-icon ${iconClass}`} onClick={toggleViewPassword} />
     </>
   );
 };
