78920b458acfe98616386e69ff3b19a9f1d4f7a6
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-05 00:17:12 +0700

    fix: changed to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // для github-pages заменить на HashRouter
 import "./index.css";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
