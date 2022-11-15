12d370ba6779c663047f9225b4a574939d2eb893
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-15 22:48:42 +0700

    fix: bug fixed 100vh for mobile responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -193,6 +193,14 @@ const App = () => {
     setCardId("");
   };
 
+  const documentHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
+  }
+
+  window.addEventListener('resize', documentHeight);
+  documentHeight();
+
   return isLoadingPage ? (
     <LoadingPage />
   ) : (
